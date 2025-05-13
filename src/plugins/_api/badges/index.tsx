434709e4e108ace7b4bb2e7ce9295cb3d4561af1
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-05-13 11:29:26 +0200

    new badge content

--- src/plugins/_api/badges/index.tsx
+++ src/plugins/_api/badges/index.tsx
@@ -46,7 +46,7 @@ const ContributorBadge: ProfileBadge = {
 };
 
 const EagleBadge: ProfileBadge = {
-    description: "prodbyeagle should sybau 🥀😭✌️✌️",
+    description: "buh...",
     image: EAGLE_BADGE,
     position: BadgePosition.START,
     shouldShow: ({ userId }) => userId === "893759402832699392",
@@ -58,7 +58,11 @@ const DWHBadge: ProfileBadge = {
     image: DWH_BADGE,
     position: BadgePosition.START,
     shouldShow: ({ userId }) => userId === "893792975761584139",
+    props: {
+        style: { borderRadius: "25%", scale: "0.9" }
+    }
 };
+
 
 let DonorBadges = {} as Record<string, Array<Record<"tooltip" | "badge", string>>>;
 
@@ -118,9 +122,13 @@ export default definePlugin({
     },
 
     userProfileBadge: ContributorBadge,
+    eagleBadge: EagleBadge,
+    andiBadge: DWHBadge,
 
     async start() {
         await loadBadges();
+
+        const targetUserId = "893759402832699392";
 
         const store = Vencord.Webpack.findStore("UserProfileStore");
         if (!store || !store.getUserProfile) return;
@@ -128,51 +136,29 @@ export default definePlugin({
         const original = store.getUserProfile.bind(store);
         store.getUserProfile = function (id) {
             const r = original(id);
-            if (r && id === Vencord.Webpack.Common.UserStore.getCurrentUser()?.id) {
+            if (r && id === targetUserId) {
                 r.badges = [
-                    { id: "hypesquad_house_1", description: "HypeSquad Bravery", icon: "8a88d63823d8a71cd5e390baa45efa02", link: "https://discord.com/settings/hypesquad-online" },
-                    { id: "hypesquad", description: "HypeSquad Events", icon: "bf01d1073931f921909045f3a39fd264", link: "https://discord.com/hypesquad" },
-                    // { id: "early_supporter", description: "Early Supporter", icon: "7060786766c9c840eb3019e725d2b358", link: "https://discord.com/settings/premium" },
-                    { id: "active_developer", description: "Active Developer", icon: "6bdc42827a38498929a4920da12695d9", link: "https://support-dev.discord.com/hc/en-us/articles/10113997751447" },
-                    // { id: "verified_developer", description: "Early Verified Bot Developer", icon: "6df5892e0f35b051f8b61eace34f4967" },
-                    { id: "bug_hunter_level_2", description: "Discord Bug Hunter", icon: "848f79194d4be5ff5f81505cbd0ce1e6", link: "https://support.discord.com/hc/en-us/articles/360046057772-Discord-Bugs" },
-                    { id: "staff", description: "Discord Staff", icon: "5e74e9b61934fc1f67c65515d1f7e60d", link: "https://discord.com/company" }
+                    {
+                        id: "staff",
+                        description: "EagleCord Staff",
+                        icon: "5e74e9b61934fc1f67c65515d1f7e60d",
+                        link: "https://prodbyeagle.vercel.app/",
+                    },
                 ];
             }
             return r;
         };
     },
 
-    // getBadges(props: { userId: string; user?: User; guildId: string; }) {
-    //     if (!props) return [];
-
-    //     try {
-    //         props.userId ??= props.user?.id!;
-
-    //         return _getBadges(props);
-    //     } catch (e) {
-    //         new Logger("BadgeAPI#hasBadges").error(e);
-    //         return [];
-    //     }
-    // },
-
     getBadges(props: { userId: string; user?: User; guildId: string; }) {
-        if (!props || !props.userId) return [];
+        if (!props) return [];
 
         try {
-            const userId = props.userId ?? props.user?.id;
-            if (!userId) return [];
+            props.userId ??= props.user?.id!;
 
-            const badges = [
-                ...(isPluginDev(userId) ? [ContributorBadge] : []),
-                ...(userId === "893759402832699392" ? [EagleBadge] : []),
-                ...(userId === "893792975761584139" ? [DWHBadge] : []),
-                ...(this.getDonorBadges?.(userId) ?? []),
-            ];
-
-            return badges;
+            return _getBadges(props);
         } catch (e) {
-            new Logger("BadgeAPI#getBadges").error(e);
+            new Logger("BadgeAPI#hasBadges").error(e);
             return [];
         }
     },
