a307466189f2e81de1e9b248f6905a8efb526ecf
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-09-11 21:34:30 +0200

    Fix mistakes

--- src/plugins/noPendingCount/index.ts
+++ src/plugins/noPendingCount/index.ts
@@ -18,26 +18,25 @@ const settings = definePluginSettings({
         type: OptionType.BOOLEAN,
         description: "Hide incoming friend requests count",
         default: true,
-        restartNeeded: true,
+        restartNeeded: true
     },
     hideMessageRequestsCount: {
         type: OptionType.BOOLEAN,
         description: "Hide message requests count",
         default: true,
-        restartNeeded: true,
+        restartNeeded: true
     },
     hidePremiumOffersCount: {
         type: OptionType.BOOLEAN,
         description: "Hide nitro offers count",
         default: true,
-        restartNeeded: true,
-    },
+        restartNeeded: true
+    }
 });
 
 export default definePlugin({
     name: "NoPendingCount",
-    description:
-        "Removes the ping count of incoming friend requests, message requests, and nitro offers.",
+    description: "Removes the ping count of incoming friend requests, message requests, and nitro offers.",
     authors: [Devs.amia],
 
     settings: settings,
@@ -50,8 +49,8 @@ export default definePlugin({
             predicate: () => settings.store.hideFriendRequestsCount,
             replacement: {
                 match: /(?<=getPendingCount\(\)\{)/,
-                replace: "return 0;",
-            },
+                replace: "return 0;"
+            }
         },
         // Message requests hook
         {
@@ -59,8 +58,8 @@ export default definePlugin({
             predicate: () => settings.store.hideMessageRequestsCount,
             replacement: {
                 match: /(?<=getMessageRequestsCount\(\)\{)/,
-                replace: "return 0;",
-            },
+                replace: "return 0;"
+            }
         },
         // This prevents the Message Requests tab from always hiding due to the previous patch (and is compatible with spam requests)
         // In short, only the red badge is hidden. Button visibility behavior isn't changed.
@@ -69,8 +68,8 @@ export default definePlugin({
             predicate: () => settings.store.hideMessageRequestsCount,
             replacement: {
                 match: /(?<=getSpamChannelsCount\(\);return )\i\.getMessageRequestsCount\(\)/,
-                replace: "$self.getRealMessageRequestCount()",
-            },
+                replace: "$self.getRealMessageRequestCount()"
+            }
         },
         {
             find: "showProgressBadge:",
@@ -78,13 +77,13 @@ export default definePlugin({
             replacement: {
                 // The two groups inside the first group grab the minified names of the variables,
                 // they are then referenced later to find unviewedTrialCount + unviewedDiscountCount.
-                match: /(\{unviewedTrialCount:(\i),unviewedDiscountCount:(\i)\}.+?\i)=\1\+\2/,
-                replace: (_, rest) => `${rest}=0`,
-            },
-        },
+                match: /(\{unviewedTrialCount:(\i),unviewedDiscountCount:(\i)\}.+?)\2\+\3/,
+                replace: (_, rest) => `${rest}0`
+            }
+        }
     ],
 
     getRealMessageRequestCount() {
         return MessageRequestStore.getMessageRequestChannelIds().size;
-    },
+    }
 });
