af521fd354bc03f69f716850be6dde78af48d61d
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-10-11 02:59:19 +0200

    RevealAllSpoilers: use cmd key on macOS

--- src/plugins/revealAllSpoilers/index.ts
+++ src/plugins/revealAllSpoilers/index.ts
@@ -6,20 +6,16 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { Devs } from "@utils/constants";
+import { Devs, IS_MAC } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { findByPropsLazy } from "@webpack";
 
 const SpoilerClasses = findByPropsLazy("spoilerContent");
-const MessagesClasses = findByPropsLazy(
-    "messagesWrapper",
-    "navigationDescription",
-);
+const MessagesClasses = findByPropsLazy("messagesWrapper", "navigationDescription");
 
 export default definePlugin({
     name: "RevealAllSpoilers",
-    description:
-        "Reveal all spoilers in a message by Ctrl-clicking a spoiler, or in the chat with Ctrl+Shift-click",
+    description: "Reveal all spoilers in a message by Ctrl-clicking a spoiler, or in the chat with Ctrl+Shift-click",
     authors: [Devs.whqwert],
 
     patches: [
@@ -27,17 +23,15 @@ export default definePlugin({
             find: ".removeObscurity,",
             replacement: {
                 match: /(?<="removeObscurity",(\i)=>{)/,
-                replace: (_, event) => `$self.reveal(${event});`,
-            },
-        },
+                replace: (_, event) => `$self.reveal(${event});`
+            }
+        }
     ],
 
     reveal(event: MouseEvent) {
-        const { ctrlKey, shiftKey, target } = event;
+        const { ctrlKey, metaKey, shiftKey, target } = event;
 
-        if (!ctrlKey) {
-            return;
-        }
+        if (!(IS_MAC ? metaKey : ctrlKey)) { return; }
 
         const { spoilerContent, hidden } = SpoilerClasses;
         const { messagesWrapper } = MessagesClasses;
@@ -46,10 +40,9 @@ export default definePlugin({
             ? document.querySelector(`div.${messagesWrapper}`)
             : (target as HTMLSpanElement).parentElement;
 
-        for (const spoiler of parent!.querySelectorAll(
-            `span.${spoilerContent}.${hidden}`,
-        )) {
+        for (const spoiler of parent!.querySelectorAll(`span.${spoilerContent}.${hidden}`)) {
             (spoiler as HTMLSpanElement).click();
         }
-    },
+    }
+
 });
